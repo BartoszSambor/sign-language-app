d1cf5961ca5f0a1b609f90bca6bc088e23415e19
Bartosz Sambór <72545684+BartoszSambor@users.noreply.github.com>
2023-06-13 00:07:26 +0200

    Class for finding hand in image, image preparation

--- model_validation.py
+++ model_validation.py
@@ -4,194 +4,59 @@ import string
 import matplotlib.pyplot as plt
 from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay, accuracy_score
 from tensorflow import keras
-
+from hand_truncation import HandSearch
+from utils import letter_to_number, number_to_letter
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 import cv2
 import mediapipe as mp
 import numpy as np
 
 model = keras.models.load_model('./models/ASL/ASL.h5')
+# model = keras.models.load_model('./models/ASLv2/model1.h5')
+
+# model = keras.models.load_model('./models/mediapipe/smnist.h5')
+
+hand_finder = HandSearch()
 
 
-# input RGB image, cut and scale it for neural network
-def prepare_image(image):
-    # media pipe init
-    mphands = mp.solutions.hands
-    hands = mphands.Hands()
+# input image, cut and scale it for neural network
+# cmap: 'RGB' or 'GRAY'
+# truncate_hands finds hand and cut out it
+# equalize_value equalize histogram of third channel in HSV colorspace
+def prepare_image(image, output_size: (int, int), cmap, truncate_hands=True, equalize_value=True):
+    image = image.copy()
 
-    h, w, c = image.shape
+    if equalize_value:
+        # Convert to HSV and equalize V
+        image = cv2.cvtColor(image, cv2.COLOR_RGB2HSV)
+        h = image[:, :, 2]
+        h = cv2.equalizeHist(h)
+        image[:, :, 2] = h
+        image = cv2.cvtColor(image, cv2.COLOR_HSV2RGB)
 
-    hand_landmarks = hands.process(image).multi_hand_landmarks
-    hand = np.zeros((300, 300, 3))
-    if hand_landmarks:
-        for handLMs in hand_landmarks:
-            x_max = 0
-            y_max = 0
-            x_min = w
-            y_min = h
-            for lm in handLMs.landmark:
-                x, y = int(lm.x * w), int(lm.y * h)
-                if x > x_max:
-                    x_max = x
-                if x < x_min:
-                    x_min = x
-                if y > y_max:
-                    y_max = y
-                if y < y_min:
-                    y_min = y
-            y_min -= 40
-            y_max += 40
-            x_min -= 50
-            x_max += 50
-            if x_min < 0:
-                x_min = 0
-            if y_min < 0:
-                y_min = 0
-            cv2.rectangle(image, (x_min, y_min), (x_max, y_max), (0, 255, 0), 2)
-            # mp_drawing.draw_landmarks(frame, handLMs, mphands.HAND_CONNECTIONS)
-            hand = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-            hand = hand[y_min:y_max, x_min:x_max]
+    if truncate_hands:
+        hands = hand_finder.get_hand_image(image)
+        if hands is not None:  # If hands found
+            scaled = cv2.resize(hands, output_size)
+        else:  # If hands not found
+            scaled = cv2.resize(image, output_size)
+    else:
+        scaled = cv2.resize(image, output_size)
 
+    if cmap == "GRAY":
+        scaled = cv2.cvtColor(scaled, cv2.COLOR_RGB2GRAY)
 
-    # scaled = cv2.resize(hand, (64, 64))
-    # version without mediapipe hand recognition
-    scaled = cv2.resize(image, (64, 64))
+    # cv2.imshow("scaled", cv2.resize(scaled, (300, 300)))
+    # cv2.waitKey(100)
 
-    # TODO: I don;t know about correct type and range for input image
-    # scaled = (scaled / np.max(scaled) * 255).astype("uint8")
+    scaled = (scaled / np.max(scaled) * 255).astype("uint8")  # for uint8
+    # scaled = (scaled / np.max(scaled)).astype("float") # for float
     return scaled
 
 
-# God, please forgive me
-def letter_to_number(letter):
-    if letter == 'A':
-        number = 0
-    elif letter == 'B':
-        number = 1
-    elif letter == 'C':
-        number = 2
-    elif letter == 'D':
-        number = 3
-    elif letter == 'E':
-        number = 4
-    elif letter == 'F':
-        number = 5
-    elif letter == 'G':
-        number = 6
-    elif letter == 'H':
-        number = 7
-    elif letter == 'I':
-        number = 8
-    elif letter == 'J':
-        number = 9
-    elif letter == 'K':
-        number = 10
-    elif letter == 'L':
-        number = 11
-    elif letter == 'M':
-        number = 12
-    elif letter == 'N':
-        number = 13
-    elif letter == 'O':
-        number = 14
-    elif letter == 'P':
-        number = 15
-    elif letter == 'Q':
-        number = 16
-    elif letter == 'R':
-        number = 17
-    elif letter == 'S':
-        number = 18
-    elif letter == 'T':
-        number = 19
-    elif letter == 'U':
-        number = 20
-    elif letter == 'V':
-        number = 21
-    elif letter == 'W':
-        number = 22
-    elif letter == 'X':
-        number = 23
-    elif letter == 'Y':
-        number = 24
-    elif letter == 'Z':
-        number = 25
-    elif letter == 'del':
-        number = 26
-    elif letter == 'nothing':
-        number = 27
-    elif letter == 'space':
-        number = 28
-    else:
-        number = -1  # Unknown
-    return number
 
-
-def number_to_letter(number):
-    if number == 0:
-        label = 'A'
-    elif number == 1:
-        label = 'B'
-    elif number == 2:
-        label = 'C'
-    elif number == 3:
-        label = 'D'
-    elif number == 4:
-        label = 'E'
-    elif number == 5:
-        label = 'F'
-    elif number == 6:
-        label = 'G'
-    elif number == 7:
-        label = 'H'
-    elif number == 8:
-        label = 'I'
-    elif number == 9:
-        label = 'J'
-    elif number == 10:
-        label = 'K'
-    elif number == 11:
-        label = 'L'
-    elif number == 12:
-        label = 'M'
-    elif number == 13:
-        label = 'N'
-    elif number == 14:
-        label = 'O'
-    elif number == 15:
-        label = 'P'
-    elif number == 16:
-        label = 'Q'
-    elif number == 17:
-        label = 'R'
-    elif number == 18:
-        label = 'S'
-    elif number == 19:
-        label = 'T'
-    elif number == 20:
-        label = 'U'
-    elif number == 21:
-        label = 'V'
-    elif number == 22:
-        label = 'W'
-    elif number == 23:
-        label = 'X'
-    elif number == 24:
-        label = 'Y'
-    elif number == 25:
-        label = 'Z'
-    elif number == 26:
-        label = 'del'
-    elif number == 27:
-        label = 'nothing'
-    elif number == 28:
-        label = 'space'
-    else:
-        label = 'unknown'
-    return label
-
-
-def load_dataset():
+# cmap = "RGB" or "GRAY"
+def load_dataset(output_size: (int, int), cmap):
     folder = "./tests/daniel_my_dataset/"
     raw_data = []
     for filename in os.listdir(folder):
@@ -201,14 +66,22 @@ def load_dataset():
     images = []
     labels = []
     for image, letter in raw_data:
-        images.append(prepare_image(image).reshape(-1, 64, 64, 3))
+        if cmap == "GRAY":
+            channels = 1
+        else:
+            channels = 3
+        images.append(prepare_image(image, output_size, cmap).reshape(-1, *output_size, channels))
         labels.append(letter_to_number(letter))
 
     return images, labels
 
-
-def draw_confusion_matrix(x_test, y_test):
-    x_test = np.array(x_test).reshape((-1, 64, 64, 3))
+# cmap = "RGB" or "GRAY"
+def draw_confusion_matrix(x_test, y_test, output_size: (int, int), cmap):
+    if cmap == "GRAY":
+        channels = 1
+    else:
+        channels = 3
+    x_test = np.array(x_test).reshape((-1, *output_size, channels))
     predictions = model.predict(x_test)
     predictions = np.argmax(predictions, axis=1)
     print(accuracy_score(y_test, predictions))
@@ -218,4 +91,16 @@ def draw_confusion_matrix(x_test, y_test):
     plt.show()
 
 
-draw_confusion_matrix(*load_dataset())
+def one_by_one_test(x_test, y_test):
+    x_test = np.array(x_test).reshape((-1, 64, 64, 3))
+    predictions = model.predict(x_test)
+    predictions = np.argmax(predictions, axis=1)
+    for i in range(len(x_test)):
+        x = x_test[i]
+        cv2.imshow("img", x)
+        print(number_to_letter(predictions[i]), " expected: ", y_test[i])
+        cv2.waitKey(0)
+
+
+draw_confusion_matrix(*load_dataset((64, 64), "RGB"), (64, 64), "RGB")
+# one_by_one_test(*load_dataset())
